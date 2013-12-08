8aeafac095c4ed6d3b9e459dd9db3974a434643a
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-12-07 19:36:14 -0800

    compaction

--- wmkv.js
+++ wmkv.js
@@ -1,76 +1,123 @@
+var Bitmap = require( 'persistable-bitmap' );
+
+/**
+ * Wavelet Matrix Class
+ */
+var Wm = function() {
+
+};
+
+Wm.prototype.rank = function() {
+  // TODO
+};
+
+Wm.prototype.select = function(key) {
+  // TODO
+};
+
+Wm.prototype.add = function(keys) {
+  if (!Array.isArray(keys)) {
+    keys = [keys];
+  }
+  // TODO
+};
+
+Wm.prototype.remove = function(key) {
+  // TODO
+};
+
 /**
  * Wmkv Class
  *
  * @param Values {Class} Array like class to store data, e.g. typed array; default: Array
+ * @param maxCacheSize {Number} if the number of entries exceed the maxCacheSize, recreate the wavelet matrix; default: 1024
  */
-var Wmkv = module.exports = function(Values) {
-  if (!Values) {
-    Values = Array;
-  }
+var Wmkv = module.exports = function(Values, maxCacheSize) {
   if (! (this instanceof Wmkv)) { // enforcing new
-    return new Wmkv(Values);
+    return new Wmkv(Values, maxCacheSize);
   }
-  this._Values = Values;
-  this._kv = {};
+  this._Values = Values || Array;
+  this._wmkv = {};
   // {
   //  1d: new this._Values(), // data indexed by the position of the key
   //  1m: typed array, // wavelet matrix
   //  2d: ...,
   //  2m: ...,
   //  ...
+  this._cacheKv = {}; // cache key value
+  this._maxCacheSize = maxCacheSize || 1024;
+  this._cacheSize = 0;
 };
 
 Wmkv.prototype._append = function(key, val) {
   var l = key.length;
   var d = l + 'd';
   var m = l + 'm';
-  var _kv = this._kv;
-  var vals = _kv[d];
+  var _wmkv = this._wmkv;
+  var vals = _wmkv[d];
   var pos = vals.length;
   vals[pos] = val;
-  var wm = _kv[m];
-  wm[pos] = new Buffer(key); // TODO
-};
-
-Wmkv.prototype._rank = function() {
-  // TODO
-};
-
-Wmkv.prototype._select = function(key) {
-  // TODO
+  var wm = _wmkv[m];
+  wm.add(key); // TODO
 };
 
 Wmkv.prototype.get = function(key) {
+  var _cacheKv = this._cacheKv;
+  if (_cacheKv.hasOwnProperty(key)) {
+    return _cacheKv[key];
+  }
   var l = key.length;
   var d = l + 'd';
   var m = l + 'm';
-  var _kv = this._kv;
-  var pos = this._select(key);
-  return (pos--) ? _kv[d][pos] : undefined;
+  var _wmkv = this._wmkv;
+  var pos = _wmkv[m].select(key);
+  return (pos--) ? _wmkv[d][pos] : undefined;
 };
 
 Wmkv.prototype.set = function(key, val) {
-  var l = key.length;
-  var d = l + 'd';
-  var m = l + 'm';
-  var _kv = this._kv;
-  if (!_kv.hasOwnProperty(d)) {
-    _kv[d] = new this._Values();
-    _kv[m] = []; // row: key, col: bits
+  var _cacheKv = this._cacheKv;
+  if (!_cacheKv.hasOwnProperty(key)) {
+    this._cacheSize += 1;
   }
-  var pos = this._select(key);
-  if (pos--) {
-    _kv[d][pos] = val;
-  } else {
-    this._append(key, val);
+  _cacheKv[key] = val;
+  if (this._cacheSize > this._maxCacheSize) {
+    this.compaction();
   }
 };
 
 Wmkv.prototype.del = function(key) {
+  var _cacheKv = this._cacheKv;
+  if (_cacheKv.hasOwnProperty(key)) {
+    _cacheKv[key] = undefined;
+  }
+  // No need to update this._wmkv since the cache keeps key: undefined
+};
+
+/**
+ * Move all cache data to the wavelet matrix
+ */
+Wmkv.prototype.compaction = function() {
+  var l = key.length;
+  var d = l + 'd';
+  var m = l + 'm';
+  var _wmkv = this._wmkv;
+  if (!_wmkv.hasOwnProperty(d)) {
+    _wmkv[d] = new this._Values();
+    _wmkv[m] = []; // row: key, col: bits
+  }
+  var pos = this._select(key);
+  if (pos--) {
+    _wmkv[d][pos] = val;
+  } else {
+    this._append(key, val);
+  }
   // TODO
+  this._cacheKv = {};
+  this._cacheSize = 0;
 };
 
 Wmkv.prototype.serialize = function() {
+  this.compaction();
   // TODO
 };
 
